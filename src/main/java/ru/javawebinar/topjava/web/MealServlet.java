cafd5a5bacdae7faeb7f6344fdd94f0d54127aca
Дмитрий <gladyshev.dv@mail.ru>
2019-06-08 22:50:17 +0300

    1.1. Creating meals2

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,12 +2,14 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
@@ -15,6 +17,8 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-        response.sendRedirect("meals.jsp");
+        request.setAttribute("list", MealsUtil.getFilteredWithExcess(MealsUtil.getMeals(),
+                LocalTime.MIN, LocalTime.MAX, 2000));
+        request.getRequestDispatcher("meals.jsp").forward(request, response);
     }
 }

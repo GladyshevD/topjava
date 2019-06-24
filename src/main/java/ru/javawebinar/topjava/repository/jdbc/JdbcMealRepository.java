cbba1a62eb34a3db303bf8d95b572737af55ff9a
Дмитрий <gladyshev.dv@mail.ru>
2019-06-24 21:00:58 +0300

    HW03 Base Task

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -0,0 +1,77 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public class JdbcMealRepository implements MealRepository {
+    private static final BeanPropertyRowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private final SimpleJdbcInsert insertMeal;
+
+    @Autowired
+    public JdbcMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.insertMeal = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id");
+
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    @Override
+    public Meal save(Meal meal, int userId) {
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("description", meal.getDescription())
+                .addValue("datetime", meal.getDateTime())
+                .addValue("calories", meal.getCalories())
+                .addValue("user_id", userId);
+        if (meal.isNew()) {
+            Number newKey = insertMeal.executeAndReturnKey(mapSqlParameterSource);
+            meal.setId(newKey.intValue());
+        } else if (namedParameterJdbcTemplate.update(
+                "UPDATE meals SET description=:description, datetime=:datetime, " +
+                        "calories=:calories, user_id=:user_id WHERE id=:id", mapSqlParameterSource) == 0) {
+            return null;
+        }
+        return meal;
+    }
+
+    @Override
+    public boolean delete(int id, int userId) {
+        return jdbcTemplate.update("DELETE FROM meals WHERE id=?", id) != 0;
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        List<Meal> meals = jdbcTemplate.query("SELECT * FROM meals WHERE id=?", ROW_MAPPER, id);
+        return DataAccessUtils.singleResult(meals);
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id = ? ORDER BY datetime DESC", ROW_MAPPER, userId);
+    }
+
+    @Override
+    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
+        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id = ? AND datetime >= ? AND datetime <= ? ORDER BY datetime DESC",
+                ROW_MAPPER, userId, startDate, endDate);
+    }
+}
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,11 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, datetime, calories, user_id) VALUES
+('Завтрак', '2019-05-30 10:00', 500, 100000),
+('Обед', '2019-05-30 13:00', 1000, 100000),
+('Ужин', '2019-05-30 20:00', 500, 100000),
+('Завтрак', '2019-05-31 10:00', 1000, 100000),
+('Обед', '2019-05-3 13:001', 500, 100000),
+('Ужин', '2019-05-31 20:00', 510, 100000);
